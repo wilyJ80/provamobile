de04a262d87cda0126cc0ed292993ffa8aa93700
wilyJ80 <abjurandam@gmail.com>
2025-09-23 11:39:04 -0300

    feat: exit btn

---
+++ app/src/main/java/com/example/provaset23/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.provaset23;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button exitBtn = (Button) findViewById(R.id.exit_btn);
+        exitBtn.setOnClickListener(v -> {
+            finish();
+        });
+    }
+}
